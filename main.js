6c58966d757cd26cb9f72f23b6ccb25a3fd37d79
GabrielOJorge <gabrieloliveiraj331@gmail.com>
2021-12-16 15:01:26 -0300

    Create event that fires when bill input is filled

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 const bill = document.querySelector("#bill");
+const btn5 = document.querySelector("#btn-5");
+const btn10 = document.querySelector("#btn-10");
+const btn15 = document.querySelector("#btn-15");
+const btn25 = document.querySelector("#btn-25");
+const btn50 = document.querySelector("#btn-50");
+const customTip = document.querySelector("#custom-tip");
+const numberOfPeople = document.querySelector("#n-people");
+const tipAmountPara = document.querySelector("tip-amount");
+const totalAmountPara = document.querySelector("total-amount");
+
+
+let billValue = 0;
+let tipPercent = 0;
+
+bill.oninput = (() => {
+  billValue = bill.value;
+});
